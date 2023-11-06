02748ebc73fe730d3c23bfb198d60e7f3a8dd778
Daniel Roman Garcia <daniromanpt@gmail.com>
2023-11-06 11:39:34 +0100

    test: add core view test

--- core/tests/test_core.py
+++ core/tests/test_core.py
@@ -1,10 +1,18 @@
-
+from django.urls import resolve
+from django.test import Client
 from playwright.sync_api import Page, expect
+from core import views
 
+import django
+django.setup()
 
-def test_prueba(page: Page):
-    page.goto("https://playwright.dev/")
-    # Expect a title "to contain" a substring.
-    expect(page).to_have_title("Fast and reliable end-to-end testing for modern web apps | Playwright")
+def test_root_url_resolves_to_home_page_view():
+    res = resolve('/')
+    assert res.func == views.home_page
 
-    
+def test_home_page_returns_correct_html():
+    client = Client()
+    res = client.get('/')
+    cont = res.content
+    assert b'<html>' in cont
+    assert b'</html>' in cont
