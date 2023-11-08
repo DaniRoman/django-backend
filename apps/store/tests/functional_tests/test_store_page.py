de54b080319e1eda93cce5ba65c06b2efc4482df
Daniel Roman Garcia <daniromanpt@gmail.com>
2023-11-08 02:20:54 +0100

    tests: refactored store test && added functional test to store app

--- apps/store/tests/functional_tests/test_store_page.py
+++ apps/store/tests/functional_tests/test_store_page.py
@@ -1,4 +1,17 @@
+import subprocess
+import pytest
+
 from playwright.sync_api import Page, expect
 
+
+@pytest.fixture(scope='module', autouse=True)
+def run_django_server():
+    command = ['python3', 'manage.py', 'runserver', '0.0.0.0:8000']
+    proces = subprocess.Popen(command)
+    yield
+    proces.terminate()
+
 def test_home_page(page: Page):
-    pass
+    url = 'django-app/api/store'
+    page.goto(url)
+    expect(page.get_by_role("heading", name="Sample Store")).to_be_visible()
--- apps/store/tests/unit_tests/test_store.py
+++ apps/store/tests/unit_tests/test_store.py
@@ -5,22 +5,21 @@ from apps.store import views
 import django
 django.setup()
 
+class TestStoreMain(TestCase):
 
-def test_root_url_resolves_to_home_page_view():
-    res = resolve('/api/store/')
-    assert res.func == views.home_page
+    def test_root_url_resolves_to_home_page_view(self):
+        res = resolve('/api/store/')
+        assert res.func == views.home_page
 
-def test_home_page_returns_correct_html():
-    
-    client = Client()
-    url = reverse('store')
-    res = client.get(url)
-    # self.assertTemplateUsed(res, 'home.html')
-    template_name_list = [template for template in res.templates]
-    print(template_name_list)
-
-    assert 'index' in template_name_list
-
+    def test_home_page_returns_correct_template(self):
+        client = Client()
+        url = reverse('store')
+        res = client.get(url)
+        self.assertTemplateUsed('index.html')
+               
+        assert res.status_code == 200
+ 
 
 
-    
+
+        
